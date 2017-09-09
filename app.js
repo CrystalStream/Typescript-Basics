8639df4b49c17a5fb7644f96cf80e9db8d44a3ac
Cristofer Flores <cristofer.flort@gmail.com>
2017-09-09 09:02:04 -0500

    Interfaces in Typescript

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log("It works");
+function greet(person) {
+    console.log(person.firstName);
+}
+var person = {
+    firstName: "Christofer",
+    age: 22,
+    hobbies: ["Programming", "Games"],
+    greet: function (lastName) {
+        console.log("Hello " + this.firstName + " " + lastName);
+    }
+};
+greet(person); // this will work cuz we are passing and object with at least the property we define on the type assignment
+var doubleFunction;
+doubleFunction = function (n1, n2) {
+    return n1 * n2;
+};
+var oldPerson = {
+    age: 20,
+    firstName: 'Christofer',
+    greet: function (s) {
+        console.log("Hi " + s);
+    }
+};
--- app.ts
+++ app.ts
@@ -1,1 +1,48 @@
-console.log("It works");
+interface NamedPerson {
+	firstName: string;
+	[propName: string]: any; // We are able to add something else to the interface for fexible properties
+	age?: number; // Optional property
+	greet(lastName: string): void;
+}
+
+function greet(person: NamedPerson){  // we can define the params { name: string } we define a type and with that we become that to be an interface
+	console.log(person.firstName)
+}
+
+const person = {
+	firstName: "Christofer",
+	age: 22,
+	hobbies: ["Programming", "Games"],
+	greet(lastName: string){
+		console.log(`Hello ${this.firstName} ${lastName}`);
+	}
+}
+
+greet(person) // this will work cuz we are passing and object with at least the property we define on the type assignment
+
+
+// Function types
+
+interface DoubleValueFunc {
+	(n1: number, n2: number): number;
+}
+
+let doubleFunction: DoubleValueFunc;
+doubleFunction = function(n1: number, n2: number): number{
+	return n1 * n2;
+}
+
+
+// Interface Innheritance
+
+interface AgePerson extends NamedPerson {
+	age: number;
+}
+
+const oldPerson: AgePerson = {
+	age: 20,
+	firstName: 'Christofer',
+	greet(s: string) {
+		console.log(`Hi ${s}`);
+	}
+}
