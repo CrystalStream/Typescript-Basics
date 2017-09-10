64c209a5fc42ebeecae4f404e502fcf2f4783129
Cristofer Flores <cristofer.flort@gmail.com>
2017-09-10 10:15:44 -0500

    Decorators for classes, methods and properties

--- app.js
+++ app.js
@@ -1,2 +1,110 @@
 "use strict";
-console.log("It works");
+// Decorator
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+function logged(constructorFn) {
+    console.log("constructorFn", constructorFn);
+}
+var Person = /** @class */ (function () {
+    function Person() {
+        console.log("Hi");
+    }
+    Person = __decorate([
+        logged
+    ], Person);
+    return Person;
+}());
+// Factory
+function logging(v) {
+    return v ? logged : null;
+}
+// @logging(true)
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    return Car;
+}());
+// Advanced
+function printable(constructorFn) {
+    constructorFn.prototype.print = function () {
+        console.log(this);
+    };
+}
+// @logging(true)
+var Plant = /** @class */ (function () {
+    function Plant() {
+        this.name = 'Green Plant';
+    }
+    Plant = __decorate([
+        printable
+    ], Plant);
+    return Plant;
+}());
+var newPlant = new Plant();
+newPlant.print();
+// Method Decorator /  Property Decortator
+// This decorator return a function that took 3 parameters, the function target, the property name and the Property Descriptor
+function editable(v) {
+    return function (target, propName, descriptor) {
+        descriptor.writable = v;
+    };
+}
+function overwritable(v) {
+    return function (target, propName) {
+        var newDescriptor = {
+            writable: v
+        };
+        return newDescriptor;
+    };
+}
+var Project = /** @class */ (function () {
+    function Project(name) {
+        this.projectName = name;
+    }
+    Project.prototype.calcBudget = function () {
+        console.log(10000);
+    };
+    __decorate([
+        overwritable(true)
+    ], Project.prototype, "projectName", void 0);
+    __decorate([
+        editable(false)
+    ], Project.prototype, "calcBudget", null);
+    return Project;
+}());
+var p = new Project('TYSE');
+p.calcBudget();
+console.log(p);
+// Parameter Decorator
+function printInfo(target, methodName, paramIndex) {
+    console.log('Target', target);
+    console.log('methodName', methodName);
+    console.log('paramIndex', paramIndex);
+}
+var Course = /** @class */ (function () {
+    function Course(name) {
+        this.name;
+    }
+    Course.prototype.printStudentNumbers = function (mode, printAll) {
+        if (printAll) {
+            console.log(10000);
+        }
+        else {
+            console.log(20000);
+        }
+    };
+    __decorate([
+        __param(1, printInfo)
+    ], Course.prototype, "printStudentNumbers", null);
+    return Course;
+}());
+var course = new Course('Math');
+course.printStudentNumbers('something', true);
+course.printStudentNumbers('something', false);
--- app.ts
+++ app.ts
@@ -1,1 +1,134 @@
-console.log("It works");
+// Decorator
+
+function logged(constructorFn: Function){  // if you attach a decorator to a function you have to pass de constructor
+    console.log("constructorFn", constructorFn);
+
+} 
+
+@logged
+class Person {
+	constructor(){
+		console.log("Hi");
+	}
+}
+
+// Factory
+function logging(v: boolean){ // This parameters can be decided by developer if we uses as factory
+	return v ? logged : null;
+}
+
+// @logging(true)
+class Car {
+
+}
+
+// Advanced
+
+function printable(constructorFn: Function){
+	constructorFn.prototype.print = function (){
+		console.log(this)
+	}
+}
+
+// @logging(true)
+@printable
+class Plant {
+	name = 'Green Plant';
+}
+
+const newPlant = new Plant();
+(<any>newPlant).print();
+
+
+// Method Decorator /  Property Decortator
+
+// This decorator return a function that took 3 parameters, the function target, the property name and the Property Descriptor
+function editable(v: boolean): Function{
+	return function(target: any, propName: string, descriptor: PropertyDescriptor){ 
+		descriptor.writable = v;
+	}
+}
+
+function overwritable(v: boolean): Function{
+	return function(target: any, propName: string): any{
+		const newDescriptor: PropertyDescriptor = {
+			writable: v
+		}
+		return newDescriptor;
+	}
+}
+
+class Project {
+	@overwritable(true)
+	projectName: string;
+
+	constructor(name: string){
+		this.projectName = name
+	}
+
+	@editable(false)
+	calcBudget(){
+		console.log(10000)
+	}
+}
+
+const p = new Project('TYSE');
+p.calcBudget();
+
+console.log(p);
+
+// Parameter Decorator
+
+function printInfo(target: any, methodName: string, paramIndex: number){
+	console.log('Target', target)
+	console.log('methodName', methodName)
+	console.log('paramIndex', paramIndex)
+}
+
+
+class Course{
+	name: string;
+
+	constructor(name: string){
+		this.name;
+	}
+
+	printStudentNumbers(mode: string,  @printInfo printAll: boolean){
+		if (printAll) {
+			console.log(10000)
+		}else{
+			console.log(20000)
+		}
+	}
+}
+
+const course = new Course('Math');
+course.printStudentNumbers('something', true);
+course.printStudentNumbers('something', false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
